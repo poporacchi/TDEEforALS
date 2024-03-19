e059aa08c7e877f0b130b09ee66e3d2a6b687f64
poporacchi <kota.np+github@gmail.com>
2024-03-19 12:33:02 +0900

    20240319_release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "app.net.km.and.tdeeforals"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.2"
+        versionCode = 8
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
